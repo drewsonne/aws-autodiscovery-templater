4d8e81b1edf52866668e71fd01c838655851be23
Drew J. Sonne <drew.sonne@nordcloud.com>
2015-11-27 10:51:52 +0100

    Featuer bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aws-autodiscovery-templater',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     install_requires=['boto3','jinja2'],
@@ -13,5 +13,5 @@ aws-autodiscovery-templater=awsautodiscoverytemplater.cli_wrapper:run
     author_email='drew.sonne@gmail.com',
     author='Drew J. Sonne',
     url='https://github.com/drewsonne/aws-autodiscovery-templater',
-    download_url = 'https://github.com/drewsonne/aws-autodiscovery-templater/tarball/1.0.0', # I'll explain this in a second
+    download_url = 'https://github.com/drewsonne/aws-autodiscovery-templater/tarball/1.0.1', # I'll explain this in a second
 )
