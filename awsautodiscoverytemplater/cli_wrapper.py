7fa48e7bdd6850e25111901c4c3207f2cca161a4
Drew J. Sonne <drew.sonne@nordcloud.com>
2016-02-12 21:02:59 +0000

    Added documentation.

--- awsautodiscoverytemplater/cli_wrapper.py
+++ awsautodiscoverytemplater/cli_wrapper.py
@@ -1,11 +1,20 @@
+# coding=utf-8
+"""
+Entrypoint allowing command to be run
+"""
 from awsautodiscoverytemplater import parse_cli_args_into
 
 __author__ = 'drews'
 
 
 def run():
+    """
+    Entrypoint for cli utility
+    :return:
+    """
     templater = parse_cli_args_into()
     templater.run()
+
 
 if __name__ == '__main__':
     run()
--- awsautodiscoverytemplater/command.py
+++ awsautodiscoverytemplater/command.py
@@ -1,3 +1,7 @@
+# coding=utf-8
+"""
+Command classes
+"""
 import argparse
 import json
 import os
